98935374fca8863831dc2330a490bb541f3480d5
Kai Moritz <kai@juplo.de>
2014-01-15 12:11:21 +0100

    LessCssImportPreProcessor handles nested import-statements and media-queries
    In LESS, it is allowed, that import-statements are nested in media-queries.
    To avoid corrumption of the LESS-code, the imported code has to be placed
    exactly, where the import-statements was noted.
    
    LESS automatically generates according media-queries, if an import-statment
    has a media-query. To mimic this behaviour, the LessCssImportPreProcessor
    now nests the imported code inside an according media-query, if an import-
    statement has a media-query.
    
    TODO:
    CssImportPreProcessor should be aware of media-queries as well. Otherwise,
    it will corrupt CSS-code with import-statements, that are using media-
    queries, as shown in the result of the added test-case
    testImportWithMediaQuery.css

--- wro4j-core/src/main/java/ro/isdc/wro/model/resource/processor/impl/css/LessCssImportPreProcessor.java
+++ wro4j-core/src/main/java/ro/isdc/wro/model/resource/processor/impl/css/LessCssImportPreProcessor.java
@@ -1,8 +1,18 @@
 package ro.isdc.wro.model.resource.processor.impl.css;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ro.isdc.wro.model.resource.Resource;
 
 import ro.isdc.wro.model.resource.processor.support.LessCssImportInspector;
+import ro.isdc.wro.model.resource.processor.support.ProcessingCriteria;
+import ro.isdc.wro.model.resource.processor.support.ProcessingType;
+import ro.isdc.wro.util.WroUtil;
 
 
 /**
@@ -14,8 +24,58 @@ import ro.isdc.wro.model.resource.processor.support.LessCssImportInspector;
  */
 public class LessCssImportPreProcessor
     extends CssImportPreProcessor {
+  private static final Logger LOG = LoggerFactory.getLogger(LessCssImportPreProcessor.class);
 
   public static final String ALIAS = "lessCssImport";
+
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected String doTransform(final String cssContent, final String resourceUri)
+      throws IOException {
+    final StringBuffer sb = new StringBuffer();
+    final List<Resource> imports = new ArrayList<Resource>();
+    LessCssImportInspector inspector = new LessCssImportInspector(cssContent);
+    final Matcher m = inspector.getMatcher();
+    int pos = 0;
+     while (m.find()) {
+      sb.append(cssContent.substring(pos, m.start()));
+      pos = m.end();
+      boolean mediaQuery = !m.group(4).isEmpty();
+      if (mediaQuery) {
+        LOG.debug("Media-Query found: {}", m.group(4));
+        sb.append("@media");
+        sb.append(m.group(4));
+        sb.append("{");
+      }
+      final Resource importedResource = createImportedResource(resourceUri, inspector.extractImportUrl(m));
+      // check if already exist
+      if (imports.contains(importedResource)) {
+        LOG.debug("[WARN] Duplicate imported resource: {}", importedResource);
+      } else {
+        imports.add(importedResource);
+        onImportDetected(importedResource.getUri());
+        List<Resource> foundImports = new ArrayList<Resource>(1);
+        foundImports.add(importedResource);
+        // for now, minimize always
+        // TODO: find a way to get minimize property dynamically.
+        sb.append(preProcessorExecutor.processAndMerge(foundImports,
+            ProcessingCriteria.create(ProcessingType.IMPORT_ONLY, false)));
+      }
+      if (mediaQuery)
+        sb.append("}");
+    }
+    sb.append(cssContent.substring(pos, cssContent.length()));
+    if (!imports.isEmpty()) {
+      LOG.debug("Imported resources found : {}", imports.size());
+    }
+    LOG.debug("imports: {}", imports);
+    LOG.debug("---\n{}\n---",sb);
+    return sb.toString();
+  }
+
   /**
    * {@inheritDoc}
    */
